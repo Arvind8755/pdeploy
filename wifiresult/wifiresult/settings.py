d7d92f6373a3a40bb088d6040c52a34518c17cb8
Arvind8755 <arvindpal87554@gmail.com>
2025-06-12 06:59:20 +0530

    frist commit

--- wifiresult/wifiresult/settings.py
+++ wifiresult/wifiresult/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_2b24xqzeleuic^1$=$jt7-@3om5@lk-9v!n-2165=jh@l9&j2
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG =  True
 DEBUG = False
-ALLOWED_HOSTS = ['18.234.77.107','localhost', '127.0.0.1','wifiresult.xyz','www.wifiresult.xyz']
+ALLOWED_HOSTS = ['54.167.134.53','localhost', '127.0.0.1','wifiresult.xyz','www.wifiresult.xyz']
 # ALLOWED_HOSTS = ['192.168.193.253']
 
 
